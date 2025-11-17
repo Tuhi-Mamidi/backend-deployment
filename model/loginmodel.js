2e5eed63cdc127c8130694de152924b442af7c42
Tuhi-Mamidi <mamidituhi2005@gmail.com>
2025-11-17 19:01:10 +0530

    Update loginmodel.js

--- model/loginmodel.js
+++ model/loginmodel.js
@@ -8,7 +8,12 @@ const userSchema = new mongoose.Schema({
    password:{
     type:String,
     required:true,
-   } 
+   } ,
+   role:{
+     type:String,
+    required:true,
+   }
 });
+
 
 module.exports = mongoose.model("Login", userSchema);
